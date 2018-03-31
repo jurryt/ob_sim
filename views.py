ca0fae24ad6df41dba1b50141af45a76270d3a1b
jur <jurryt@dds.nl>
2018-03-31 17:22:10 +0200

    Cleanup

--- views.py
+++ views.py
@@ -14,28 +14,22 @@ TODO:
 
 """
 import numpy as np
-import pandas as pd 
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 from mpl_toolkits.mplot3d import Axes3D
 
-from scipy.interpolate import griddata
 
 from pymongo import MongoClient
-from database import db_read_df, db_update_df, db_clear, db_update_grid,db_read_grid
-from settings import N, MAX_X, MAX_Y, R, BEHAVIOUR, MOVIE, INTERPOLATION
-from utils import rnd_vec, dist
-
-
-    
+from database import db_read_df, db_read_grid
+from settings import MAX_X, MAX_Y, R, BEHAVIOUR, MOVIE
+   
 # setup database
 client = MongoClient()
 db = client.world    
 
-
 df = db_read_df(db)
 xi, yi, zi = db_read_grid(db, 'world')
-# plotting
+
 # scatter plot
 fig, ax = plt.subplots(figsize=(10,7))
 ax.set_xlim(0, MAX_X)
@@ -50,13 +44,7 @@ ax3d = fig3d.add_subplot(111,projection='3d')
 xm,ym = np.meshgrid(xi,yi)
 ax3d.plot_wireframe(xm,ym,zi)
 
-#while k=='':
 def update(frame_number):
-#    for r in range(R):
-
-#    for x in range(MAX_X):
-#        for 
-#    distance_grid = zeros(MAX_X, )
     global contour #, utility
     
     # read machines information 
@@ -72,37 +60,25 @@ def update(frame_number):
     #scat.set_offsets(np.concatenate([df[['x','y']].values,df[['x_trg','y_trg']].values]))
     for c in contour.collections:
         c.remove()
-#    contour = ax.contour(utility,levels=np.linspace(0,1,num=10))
-#    contour = ax.contour(df.x,df.y,df.cost, levels=np.linspace(0,1,num=10))
-#    contour = ax.contour(df[['x','y','cost']])#, levels=np.linspace(0,1,num=10))
- #   fr=df.pivot('x','y','cost').fillna(0) 
- #   xc,yc=np.meshgrid(fr.index,fr.columns)
- #   contour = ax.contour(xc,yc,fr.values)#, levels=np.linspace(0,1,num=10))
     
     xm,ym = np.meshgrid(xi,yi)
-#xc,yc=np.meshgrid(fr.index,fr.columns)
     contour = ax.contour(xi, yi, zi)#, leve
 
     for c in ax3d.collections:
         c.remove()
-
     ax3d.plot_wireframe(xm,ym,zi)
-
     
     return scat, scat_trg, contour
-        
-#        k = input()
-#while True:
-#    loop(0)
-ani = anim.FuncAnimation(fig,update,frames=R,  repeat=True, interval=500)
-#plt.show()
+
+# repeat or make movie        
+ani = anim.FuncAnimation(fig,update,frames=R,  repeat=not MOVIE, interval=500)
+
 if MOVIE:
     ani.save(BEHAVIOUR+'.mp4')
     
-ani3d = anim.FuncAnimation(fig3d,update,frames=R,  repeat=True, interval=500)
+ani3d = anim.FuncAnimation(fig3d,update,frames=R,  repeat=not MOVIE, interval=500)
 
 if MOVIE:
     ani3d.save(BEHAVIOUR+'3D.mp4')
     
-#result = pd.concat(dfs)
     
